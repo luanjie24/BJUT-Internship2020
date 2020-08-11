429bd8256081ea2627c12210953231f07fe1b89e
annbory <58623419+annbory@users.noreply.github.com>
2020-08-11 17:03:55 +0800

    Update demo.py

--- task/张逸/demo.py
+++ task/张逸/demo.py
@@ -19,17 +19,17 @@ def main():
 			q_list = query.split()
 
 			if len(q_list) == 1:
-				print("相似詞前 100 排序")
+				print("相似词前 100 排序")
 				res = model.most_similar(q_list[0],topn = 100)
 				for item in res:
 					print(item[0]+","+str(item[1]))
 
 			elif len(q_list) == 2:
-				print("計算 Cosine 相似度")
+				print("计算 Cosine 相似度")
 				res = model.similarity(q_list[0],q_list[1])
 				print(res)
 			else:
-				print("%s之於%s，如%s之於" % (q_list[0],q_list[2],q_list[1]))
+				print("%s之于%s，如%s之于" % (q_list[0],q_list[2],q_list[1]))
 				res = model.most_similar([q_list[0],q_list[1]], [q_list[2]], topn= 100)
 				for item in res:
 					print(item[0]+","+str(item[1]))
