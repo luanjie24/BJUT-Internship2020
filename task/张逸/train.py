d44fbbd5b2fe6ab648d2360c560daaf52b54ddeb
annbory <58623419+annbory@users.noreply.github.com>
2020-08-11 17:08:18 +0800

    Update train.py

--- task/张逸/train.py
+++ task/张逸/train.py
@@ -17,21 +17,24 @@ def main():
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     sentences = word2vec.LineSentence("wiki_seg.txt")
-    #model = word2vec.Word2Vec(sentences, size=250)
-    model = word2vec.Word2Vec.load("word2vec.model")
-    #model.train(sentences, total_examples=model.corpus_count, epochs=10)
+    model = word2vec.Word2Vec(sentences, size=250)
+    #model = word2vec.Word2Vec.load("word2vec.model")  #加载模型
+    model.train(sentences, total_examples=model.corpus_count, epochs=10)
 
     #保存模型，供日后使用
-    #model.save("word2vec.model")
-    #model.wv.save_word2vec_format('./word2vec_200.bin', binary=False)
+    model.save("word2vec.model")
+    #保存特征矩阵
+    model.wv.save_word2vec_format('./word2vec_200.bin', binary=False)
+    ######参考其他项目代码，生成字典，读取内容后并没有明白作用。。
     index_dict, word_vectors = create_dictionaries(model)
     pkl_name = input("请输入保存的pkl文件名...\n")
     output = open(pkl_name + u".pkl", 'wb')
     pickle.dump(index_dict, output)  # 索引字典
     pickle.dump(word_vectors, output)  # 词向量字典
     output.close()
+    ######
 
-    #模型讀取方式
+    #模型读取方式
     # model = word2vec.Word2Vec.load("your_model_name")
 
 if __name__ == "__main__":
